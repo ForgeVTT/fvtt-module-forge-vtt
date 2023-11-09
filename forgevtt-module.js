0f7026d6dd70bdd6cffb536f6d5634a6c03bbe70
Phi <34374883+rikmarais@users.noreply.github.com>
2023-11-09 02:01:18 +0100

    Release 1.14.6

--- forgevtt-module.js
+++ forgevtt-module.js
@@ -739,7 +739,7 @@ class ForgeVTT {
                 styles: [],
                 title: "The Forge",
                 url: "https://forge-vtt.com",
-                version: "1.14.5",
+                version: "1.14.6",
                 availability: 0,
                 unavailable: false
             };
