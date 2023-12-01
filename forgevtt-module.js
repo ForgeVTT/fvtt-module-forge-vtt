2a4c37b5928a26924b7048540029a0c83423e04a
Phi <34374883+rikmarais@users.noreply.github.com>
2023-12-01 17:22:57 +0100

    Release 1.14.8

--- forgevtt-module.js
+++ forgevtt-module.js
@@ -739,7 +739,7 @@ class ForgeVTT {
                 styles: [],
                 title: "The Forge",
                 url: "https://forge-vtt.com",
-                version: "1.14.7",
+                version: "1.14.8",
                 availability: 0,
                 unavailable: false
             };
