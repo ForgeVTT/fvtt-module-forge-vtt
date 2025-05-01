ccd372672ec3fb7dae5d736da2345e590b0a6716
wyrmisis <tim.snyder.work@gmail.com>
2025-05-01 14:20:22 -0400

    feat: make ForgeVTT and ForgeAPI globally available

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,8 @@
 import { ForgeVTT } from "./ForgeVTT.mjs";
+import { ForgeAPI } from "./ForgeAPI.mjs";
 import "./hooks.mjs";
 
 ForgeVTT.setupForge();
+
+globalThis.ForgeVTT = ForgeVTT;
+globalThis.ForgeAPI = ForgeAPI;
