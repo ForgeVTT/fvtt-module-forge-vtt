beb680b0a1b1454878406c0381ebd57b9a3c3320
Etienne Baudry <webmaestro.fr@gmail.com>
2025-07-30 10:32:39 +0000

    Clean up

--- src/ForgeVTT.mjs
+++ src/ForgeVTT.mjs
@@ -253,53 +253,14 @@ export class ForgeVTT {
         request.json = async () => response;
       }
       if (response.installed) {
-        // Send a fake 100% progress report with package data vending
-        const installPackageData = ForgeVTT.isFoundryNewerThan("10") ? response.data : response;
-        const id = data.id || installPackageData.id;
-        const name = data.name || installPackageData.name;
-        if (ForgeVTT.isFoundryNewerThan("13")) {
-          console.log(`MODULE installPackage RELOAD from listener (${id || name})`);
-          game.reload();
-          return request;
-        }
-        const onProgressRsp = {
-          action: data.action,
-          id: id || name,
-          name: name || id,
-          type: data.type || "module",
-          pct: 100,
-          pkg: installPackageData,
-          step: "Package",
-          manifest: data.manifest,
-        };
-        if (ForgeVTT.isFoundryNewerThan("12")) {
-          // In v12, _onProgress expects id = manifest and step = "complete"
-          onProgressRsp.step = CONST.SETUP_PACKAGE_PROGRESS.STEPS.COMPLETE;
-          onProgressRsp.id = data.manifest;
-        } else if (ForgeVTT.isFoundryNewerThan("11")) {
-          // The term that represents the "vend" step may change with FVTT versions
-          onProgressRsp.step = CONST.SETUP_PACKAGE_PROGRESS.STEPS.VEND;
-          // v11 checks the response manifest against what is passed
-        }
-        this._onProgress(onProgressRsp);
+        this._onProgress(response);
       }
       return request;
     };
   }
 
   static _patchSetupScreen() {
-    if (ForgeVTT.isFoundryNewerThan("13")) {
-      // In v13+ we need to patch `game` to override its post method.
-      game.post = ForgeVTT.#preparePostOverride(game.post);
-
-      game._addProgressListener((progressData) => {
-        // In v13.342 the setup screen doesn't reload automatically upon module installation
-        if (progressData.action === "installPackage" && progressData.pct === 100 && progressData.pkg) {
-          console.log(`MODULE installPackage RELOAD from listener (${progressData.pkg.id})`);
-          game.reload();
-        }
-      });
-    } else if (ForgeVTT.isFoundryNewerThan("9")) {
+    if (ForgeVTT.isFoundryNewerThan("9") && !ForgeVTT.isFoundryNewerThan("13")) {
       // For v9-v12, we can patch the Setup class to override its post method.
       Setup.post = ForgeVTT.#preparePostOverride(Setup.post);
     }
