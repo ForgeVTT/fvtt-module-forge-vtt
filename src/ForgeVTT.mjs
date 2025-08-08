649e7b6dbb5e3906a52b2593f76e290665a56107
Etienne <etienne@webmaestro.fr>
2025-08-08 10:39:35 +0000

    Inject fake step on proxy side

--- src/ForgeVTT.mjs
+++ src/ForgeVTT.mjs
@@ -253,31 +253,7 @@ export class ForgeVTT {
         request.json = async () => response;
       }
       if (response.installed) {
-        // Send a fake 100% progress report with package data vending
-        const installPackageData = ForgeVTT.isFoundryNewerThan("10") ? response.data : response;
-        const id = data.id || installPackageData.id;
-        const name = data.name || installPackageData.name;
-        const onProgressRsp = {
-          ...response,
-          action: data.action,
-          id: id || name,
-          name: name || id,
-          type: data.type || "module",
-          pct: 100,
-          pkg: installPackageData,
-          step: "Package",
-          manifest: data.manifest,
-        };
-        if (ForgeVTT.isFoundryNewerThan("12")) {
-          // In v12, _onProgress expects id = manifest and step = "complete"
-          onProgressRsp.step = CONST.SETUP_PACKAGE_PROGRESS.STEPS.COMPLETE;
-          onProgressRsp.id = data.manifest;
-        } else if (ForgeVTT.isFoundryNewerThan("11")) {
-          // The term that represents the "vend" step may change with FVTT versions
-          onProgressRsp.step = CONST.SETUP_PACKAGE_PROGRESS.STEPS.VEND;
-          // v11 checks the response manifest against what is passed
-        }
-        this._onProgress(onProgressRsp);
+        this._onProgress(response);
       }
       return request;
     };
