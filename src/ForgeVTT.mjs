3eee8400644d639c650428851cea1945545526c7
Etienne <etienne@webmaestro.fr>
2025-08-11 08:09:26 +0000

    Check for pkg instead of data

--- src/ForgeVTT.mjs
+++ src/ForgeVTT.mjs
@@ -254,7 +254,7 @@ export class ForgeVTT {
         // the json data, since it can only be called once
         request.json = async () => response;
       }
-      if (response.installed || response.error) {
+      if (response.installed) {
         // Send a fake 100% progress report with package data vending
         const installPackageData = ForgeVTT.isFoundryNewerThan("10") ? response.data : response;
         const id = data.id || installPackageData.id;
@@ -265,23 +265,12 @@ export class ForgeVTT {
           return request;
         }
         const onProgressRsp = {
-          action: data.action,
-          id: id || name,
-          name: name || id,
-          type: data.type || "module",
-          pct: 100,
+          ...response,
           pkg: installPackageData,
-          step: "Package",
-          manifest: data.manifest,
         };
         if (ForgeVTT.isFoundryNewerThan("12")) {
-          // In v12, _onProgress expects id = manifest and step = "complete"
-          onProgressRsp.step = CONST.SETUP_PACKAGE_PROGRESS.STEPS.COMPLETE;
+          // In v12, _onProgress expects id = manifest
           onProgressRsp.id = data.manifest;
-        } else if (ForgeVTT.isFoundryNewerThan("11")) {
-          // The term that represents the "vend" step may change with FVTT versions
-          onProgressRsp.step = CONST.SETUP_PACKAGE_PROGRESS.STEPS.VEND;
-          // v11 checks the response manifest against what is passed
         }
         this._onProgress(onProgressRsp);
       }
@@ -296,9 +285,9 @@ export class ForgeVTT {
 
       game._addProgressListener((progressData) => {
         // In v13.342 the setup screen doesn't reload automatically upon module installation
-        console.log(`PROGRESS installPackage (${progressData.data.id}) ${progressData.pct}%`);
-        if (progressData.action === "installPackage" && progressData.pct === 100 && progressData.data) {
-          console.log(`COMPLETE installPackage (${progressData.data.id}) RELOAD`);
+        console.log(`PROGRESS installPackage (${progressData.pkg.id}) ${progressData.pct}%`);
+        if (progressData.action === "installPackage" && progressData.pct === 100 && progressData.pkg) {
+          console.log(`COMPLETE installPackage (${progressData.pkg.id}) RELOAD`);
           game.reload();
         }
       });
