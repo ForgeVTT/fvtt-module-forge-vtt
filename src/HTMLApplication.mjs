d7133f365bb11cdb0dd80527d264617abcc80324
zeel <zeel02@gmail.com>
2025-08-29 18:57:39 +0000

    Improved JSDocs with callback annotations and typedefs to improve readability and decrease line length.

--- src/HTMLApplication.mjs
+++ src/HTMLApplication.mjs
@@ -1,3 +1,5 @@
+/** @typedef {foundry.applications.types.ApplicationConfiguration} AppConfig */
+
 /**
  * A simplified implementation of `ApplicationV2` that allows for easy rendering of
  * arbitrary HTML content.
@@ -8,13 +10,28 @@ export let HTMLApplication;
 
 try {
   HTMLApplication = class extends foundry.applications.api.ApplicationV2 {
+    /** @typedef {string|Node|NodeList|HTMLTemplateElement} Renderable */
+
+    /**
+     * @callback contentCallback
+     * @param {ApplicationRenderContext} context - The rendering context
+     * @param {RenderOptions} options            - The rendering options
+     * @returns {Renderable} - The content to render
+     */
+
+    /**
+     * @callback renderCallback
+     * @param {HTMLElement} element              - The HTML element to render into
+     * @param {ApplicationRenderContext} context - The rendering context
+     * @param {RenderOptions} options            - The rendering options
+     */
+
     /**
      * @param {Object} options
-     * @param {string|(ApplicationRenderContext, RenderOptions)
-     *                  => string|Node|NodeList|HTMLTemplateElement} options.content          - The content to render, either as a string or a function
-     *                                                                                          returning something that can be resolved by {@link _replaceHTML}
-     * @param {(HTMLElement, ApplicationRenderContext, RenderOptions) => void} options.render - A callback function to execute when the application is rendered.
-     * @param {foundry.applications.types.ApplicationConfiguration} options.options           - Additional application options.
+     * @param {Renderable|contentCallback} options.content - The content to render, either as a renderable value or a function
+     *                                                       returning something that can be resolved by {@link _replaceHTML}
+     * @param {renderCallback} options.render              - A callback function to execute when the application is rendered.
+     * @param {AppConfig} options.options                  - Additional application options.
      */
     constructor({ content, render, ...options }) {
       super(options);
@@ -25,9 +42,9 @@ try {
 
     /**
      * Render an HTMLElement for the Application.
-     * @param {ApplicationRenderContext} context                    - Context data for the render operation
-     * @param {RenderOptions} options                               - Options which configure application rendering behavior
-     * @returns {Promise<string|Node|NodeList|HTMLTemplateElement>} - The content to display in the application
+     * @param {ApplicationRenderContext} context - Context data for the render operation
+     * @param {RenderOptions} options            - Options which configure application rendering behavior
+     * @returns {Promise<Renderable>}            - The content to display in the application
      */
     async _renderHTML(context, options) {
       return this.renderContent.call(this, context, options);
@@ -35,8 +52,8 @@ try {
 
     /**
      * Actions performed after any render of the Application.
-     * @param {ApplicationRenderContext} context      - Prepared context data
-     * @param {RenderOptions} options                 - Provided render options
+     * @param {ApplicationRenderContext} context - Prepared context data
+     * @param {RenderOptions} options            - Provided render options
      * @returns {Promise<void>}
      */
     async _onRender(context, options) {
@@ -45,9 +62,9 @@ try {
 
     /**
      * Replace the HTML of the application with the result provided by the rendering backend.
-     * @param {string|Node|NodeList|HTMLTemplateElement} result - The result returned by the application rendering backend
-     * @param {HTMLElement} content                                      - The content element into which the rendered result must be inserted
-     * @param {RenderOptions} options                                    - Options which configure application rendering behavior
+     * @param {Renderable} result     - The result returned by the application rendering backend
+     * @param {HTMLElement} content   - The content element into which the rendered result must be inserted
+     * @param {RenderOptions} options - Options which configure application rendering behavior
      */
     _replaceHTML(result, content, options) {
       if (typeof result === "string") content.innerHTML = result;
